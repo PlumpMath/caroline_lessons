d64a795e7c91da74ed12bdeb791f21f1d804cc3e
timsgardner <timsgardner@gmail.com>
2015-03-26 17:54:05 -0400

    some things

--- program_1.clj
+++ program_1.clj
@@ -3,3 +3,21 @@
         arcadia.hydrate
         clojure.repl
         clojure.pprint))
+
+(defn kill! [obj]
+  (let [spec (dehydrate obj)]
+    (destroy obj)
+    spec))
+
+(def cube-spec
+  (kill! (create-primitive :cube)))
+
+(defn cube-assault [n]
+  (loop [i 0]
+    (when (< i n)
+      (do
+        (hydrate
+          (deep-merge-mv
+            cube-spec
+            {:transform [{:local-position [i 0 0]}]}))
+        (recur (inc i))))))
